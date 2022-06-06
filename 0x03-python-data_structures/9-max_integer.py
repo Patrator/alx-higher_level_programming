8a5826833a6a03cbe76d0f4e5a6ca3c099b8c131
Patrator <Akindeleagbemuko@gmail.com>
2022-06-06 21:25:41 +0100

    Task 7,9 and 11 updated

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    
     if not my_list:
         return None
-
     h = my_list[0]
     for integer in my_list:
         h = integer if (integer > h) else h
