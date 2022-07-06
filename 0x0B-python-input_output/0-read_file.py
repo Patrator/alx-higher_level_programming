a578e7ab4b10d57e741e51c938308249ca694ba3
Patrator <Akindeleagbemuko@gmail.com>
2022-07-06 22:35:54 +0100

    All updated

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    0-read_file Module
+"""
+
+
+def read_file(filename=""):
+    """
+        reads a text file and prints to stdout
+        Args:
+            filename: name of file to be read
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end='')
