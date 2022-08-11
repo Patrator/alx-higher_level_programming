392fa365fffdcea78004e288036972bff5b42cb8
Patrator <Akindeleagbemuko@gmail.com>
2022-08-11 12:39:16 -0700

    Advanced task

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,11 @@
--- This script lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Lists the number of records with the same score in the table second_table.
 
-SELECT score, name FROM second_table WHERE name != 'NULL' ORDER BY score DESC;
+-- Records are ordered by descending count.
+
+SELECT `score`, COUNT(*) AS `number`
+
+FROM `second_table`
+
+GROUP BY `score`
+
+ORDER BY `number` DESC;
